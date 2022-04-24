cec59357f31471663349862c5a81516c59f9bcb4
Pedro Zafalon <71289265+ranofrolds@users.noreply.github.com>
2022-04-24 18:53:15 -0300

    final

--- Colorir.java
+++ Colorir.java
@@ -9,35 +9,34 @@ public class Colorir {
         Arrays.fill(colored,"null");
         
         for(int i=0;i< g.vertices.length;i++){
-            if(g.vertices[i].cor=="null"){
+            if(g.vertices[i].cor=="null"){//caso nao tenha cor checa os adjacentes
                 g.vertices[i].cores=checarAdj(g.vertices[i], g);
             }
-            if(g.vertices[i].cores.size()<=0){
+            if(g.vertices[i].cores.size()<=0){//se nao tiver cor disponivel para realizar a pintura
                 
                 g.vertices[i].cor="null";
                 colored[i]="null";
 
-                int j=i-1;
+                int j=i-1;//indice do vertice previamente colorido
                 
-                Vertice vert=g.vertices[j]; 
+                Vertice vert=g.vertices[j]; //realiza o loop, para encontrar o primeiro vertice anterior com a possiblidade de realizar a troca de cor, e testar uma nova combinação
                 while((vert.cores.indexOf(vert.cor)+1)>=vert.cores.size()){//{"Preto", "Vermelho",     }  cor="Preto"  indexOf(Preto)-> 0+1
                     vert.cor="null";
                     colored[vert.valor]="null";
                     j--;
-                    if(j==-1){ 
-                        System.out.println("Deu trevo Pssor!!");
-                        System.out.println("NOSSO CODIGO E HORRIVEL, PUTA MATERIA HORRIVEL LIXO!!");
+                    if(j==-1){ //se chegar no ultimo elemento e nao tive rmais combinções
+                        System.out.println("Nao foi possivel encontrar uma combinação com 4 cores!!");
                         return null;
                     }
                     vert=g.vertices[j];
                 }
 
-                vert.cor=vert.cores.get(vert.cores.indexOf(vert.cor)+1);
+                vert.cor=vert.cores.get(vert.cores.indexOf(vert.cor)+1);//pega a proxima cor e coloca como atual
                 colored[vert.valor]=vert.cor;
                 i=j;   
             }
             else{
-                g.vertices[i].cor=g.vertices[i].cores.get(0);
+                g.vertices[i].cor=g.vertices[i].cores.get(0); //a cor escolhida é a primeira disponivel
                 colored[i]=g.vertices[i].cor;
             }
         }
@@ -53,13 +52,13 @@ public class Colorir {
         disponivel.add("Branco");
         disponivel.add("Vermelho");
 
-        for (int adj : g.adj.listaAdj[vertice.valor]) {
-            if(g.vertices[adj].cor!="null" && (disponivel.indexOf((g.vertices[adj].cor))!=-1)){
+        for (int adj : g.adj.listaAdj[vertice.valor]) {//checa a cor de todos os adjacentes
+            if(g.vertices[adj].cor!="null" && (disponivel.indexOf((g.vertices[adj].cor))!=-1)){//se existir cor, e ainda nao tiver sido retirada
                 disponivel.remove(disponivel.indexOf((g.vertices[adj].cor)));
             }
         }
 
-        return disponivel;
+        return disponivel;//retorna cores disponiveis para colorior o vertice
     }
 }
 
--- Main.java
+++ Main.java
@@ -5,30 +5,36 @@ import grafo.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int n = 61; // numero do grafo(arquivo)
+        int n =28 ; // numero do grafo(arquivo)
         ArrayList<String> cores = new ArrayList<String>();
         cores.add("Preto");
         cores.add("Verde");
         cores.add("Branco");
         cores.add("Vermelho");
         Main main = new Main();
-        if (n >= 0 && n <= 61) {
-            for(int i=1; i<=n;i++){
-                System.out.println("GRAFO NUMERO = "+i);
-                String fileName = Integer.toString(i) + "_grafo.txt";
+        if (n >= 0 && n <= 61) {//61 pois criamos 2 grafos para testar o codigo
+            // for(n=1; i<=n;i++){ //realizar coloração de todos os grafos
+                System.out.println("GRAFO NUMERO = "+n);
+                System.out.println("Lendo o grafo...");
+                String fileName = Integer.toString(n) + "_grafo.txt";
                 Grafo g = new Grafo();
                 LerGrafo read = new LerGrafo(g, fileName);
                 // main.printarListaAdj(g);
-
-
+                System.out.println("Grafo acessado. Realizando a coloração...");
                 Colorir col = new Colorir();
                 String[] result= col.colorir(g);
                 if(result!=null){
+                    System.out.println("Grafo colorido com sucesso, segue as combinações:");
+                    int j=0;
                     for (String string : result) {
-                        System.out.println(string);
+                        System.out.println((j+1)+" - "+string);
+                        j++;
                     }
                 }
-            }
+                else{
+                    System.out.println("Retorno = null");
+                }
+            // }
             
             
 
